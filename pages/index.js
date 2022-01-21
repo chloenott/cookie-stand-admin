3b6a74efc612194bf454a45fa6e7e9edeeb8a400
Chloe Nott <chloe@nott.email>
2022-01-20 20:33:11 -0800

    Added readme description for lab 37.

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
-  let data = `{"location":"Barcelona","minCustomers":2,"maxCustomers":4,"avgCookies":2.5}`
-
   return (
     <div>
       <Head>
@@ -18,36 +16,7 @@ export default function Home() {
           </h1>
       </header>
 
-      <main className="p-6 bg-emerald-50">
-        <form className="w-8/12 p-3 mx-auto text-sm rounded-lg bg-emerald-300">
-          <h2 className="text-xl text-center">Create Cookie Stand</h2>
-
-          <div className="flex m-2">
-            <label className="pr-2">Location</label>
-            <input name="location" value="Barcelona" className="flex-grow bg-blue-100"></input>
-          </div>
-
-          <div className="flex mt-5">
-            <div className="w-1/4 mx-3 my-2">
-              <label className="block mx-auto text-center">Minimum Customers per Hour</label>
-              <input name="min_cust_per_hour" value="2" className="w-full"></input>
-            </div>
-            <div className="w-1/4 mx-3 my-2">
-              <label className="block mx-auto text-center">Maximum Customers per Hour</label>
-              <input name="max_cust_per_hour" value="2" className="w-full"></input>
-            </div>
-            <div className="content-center w-1/4 mx-3 my-2">
-              <label className="block text-center">Average Cookies per Sale</label>
-              <input name="avg_cookies_per_sale" value="2" className="w-full"></input>
-            </div>
-            <button className="flex-grow w-1/4 mx-auto bg-emerald-500">Create</button>
-          </div>
-        </form>
-        <div className="w-8/12 m-auto text-sm text-center text-gray-500">
-          <p className="p-6">Report Table Coming Soon...</p>
-          <p>{ data }</p>
-        </div>
-      </main>
+      <Main></Main>
 
       <footer className="px-5 py-3 bg-emerald-500">
         <p className="text-sm">©2022</p>
@@ -56,3 +25,51 @@ export default function Home() {
   )
 }
 
+function Main() {
+  return (
+    <main className="p-6 bg-emerald-50">
+    <Form></Form>
+    <ReportTable></ReportTable>
+
+  </main>
+  )
+}
+
+function Form() {
+  return (
+    <form className="w-8/12 p-3 mx-auto text-sm rounded-lg bg-emerald-300">
+      <h2 className="text-xl text-center">Create Cookie Stand</h2>
+
+      <div className="flex m-2">
+        <label className="pr-2">Location</label>
+        <input name="location" value="Barcelona" className="flex-grow bg-blue-100"></input>
+      </div>
+
+      <div className="flex mt-5">
+        <div className="w-1/4 mx-3 my-2">
+          <label className="block mx-auto text-center">Minimum Customers per Hour</label>
+          <input name="min_cust_per_hour" value="2" className="w-full"></input>
+        </div>
+        <div className="w-1/4 mx-3 my-2">
+          <label className="block mx-auto text-center">Maximum Customers per Hour</label>
+          <input name="max_cust_per_hour" value="2" className="w-full"></input>
+        </div>
+        <div className="content-center w-1/4 mx-3 my-2">
+          <label className="block text-center">Average Cookies per Sale</label>
+          <input name="avg_cookies_per_sale" value="2" className="w-full"></input>
+        </div>
+        <button className="flex-grow w-1/4 mx-auto bg-emerald-500">Create</button>
+      </div>
+    </form>
+  )
+}
+
+function ReportTable() {
+  let data = `{"location":"Barcelona","minCustomers":2,"maxCustomers":4,"avgCookies":2.5}`
+  return (
+    <div className="w-8/12 m-auto text-sm text-center text-gray-500">
+      <p className="p-6">Report Table Coming Soon...</p>
+      <p>{ data }</p>
+    </div>
+  )
+}
