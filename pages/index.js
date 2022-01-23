a03fb39f945525c2863d6ab3dd7a8d146b40cf92
Chloe Nott <chloe@nott.email>
2022-01-23 02:28:26 -0800

    Added overview button to header. The button does nothing.

--- pages/index.js
+++ pages/index.js
@@ -32,10 +32,11 @@ export default function CookieStandAdmin() {
 
 function Header({title}) {
   return (
-    <header className="px-5 py-3 bg-emerald-500">
-      <h1 className="text-3xl">
+    <header className="flex justify-between px-5 py-3 bg-emerald-500">
+      <h1 className="inline-block text-3xl">
         {title}
       </h1>
+      <button className="inline-block px-2 py-0 bg-gray-100 rounded">Overview</button>
     </header>
   )
 }
