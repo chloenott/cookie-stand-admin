94fabc0b08a7ee66ef50a6b4655659adcc75708a
Chloe Nott <chloe@nott.email>
2022-01-22 23:33:57 -0800

    Refactored code to fit into new Header, Footer, CreateForm, ReportTable, and CookieStandAdmin components. Adjusted styling to match spec.

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,70 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { render } from 'react-dom'
 
-export default function Home() {
-
+export default function CookieStandAdmin() {
   return (
     <div>
       <Head>
         <title>Cookie Stand Admin</title>
       </Head>
 
-      <header className="px-5 py-3 bg-emerald-500">
-          <h1 className="text-3xl">
-            Cookie Stand Admin
-          </h1>
-      </header>
+      <Header title="Cookie Stand Admin" />
 
-      <Main></Main>
+      <main className="p-6 bg-emerald-50">
+        <CreateForm />
+        <ReportTable />
+      </main>
 
-      <footer className="px-5 py-3 bg-emerald-500">
-        <p className="text-sm">©2022</p>
-      </footer>
+      <Footer />
     </div>
   )
 }
 
-function Main() {
+function Header({title}) {
   return (
-    <main className="p-6 bg-emerald-50">
-    <Form></Form>
-    <ReportTable></ReportTable>
-
-  </main>
+    <header className="px-5 py-3 bg-emerald-500">
+      <h1 className="text-3xl">
+        {title}
+      </h1>
+    </header>
   )
 }
 
-function Form() {
+function Footer() {
   return (
-    <form className="w-8/12 p-3 mx-auto text-sm rounded-lg bg-emerald-300">
-      <h2 className="text-xl text-center">Create Cookie Stand</h2>
+    <footer className="px-5 py-3 bg-emerald-500">
+      <p className="text-sm">©2022</p>
+    </footer>
+  )
+}
+
+function CreateForm() {
+  return (
+    <form className="w-8/12 px-3 pb-2 mx-auto text-sm rounded-lg bg-emerald-300">
+      <h2 className="p-3 text-xl text-center">Create Cookie Stand</h2>
 
       <div className="flex m-2">
         <label className="pr-2">Location</label>
-        <input name="location" value="Barcelona" className="flex-grow bg-blue-100"></input>
+        <input name="location" value="" className="flex-grow"></input>
       </div>
 
       <div className="flex mt-5">
-        <div className="w-1/4 mx-3 my-2">
+        <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block mx-auto text-center">Minimum Customers per Hour</label>
           <input name="min_cust_per_hour" value="2" className="w-full"></input>
         </div>
-        <div className="w-1/4 mx-3 my-2">
+        <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block mx-auto text-center">Maximum Customers per Hour</label>
           <input name="max_cust_per_hour" value="2" className="w-full"></input>
         </div>
-        <div className="content-center w-1/4 mx-3 my-2">
+        <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block text-center">Average Cookies per Sale</label>
           <input name="avg_cookies_per_sale" value="2" className="w-full"></input>
         </div>
-        <button className="flex-grow w-1/4 mx-auto bg-emerald-500">Create</button>
+        <div className="flex items-center justify-center w-1/4 p-2 mx-1 my-2 rounded bg-emerald-500">
+          <button className="w-1/4 mx-1 bg-emerald-500">Create</button>
+        </div>
       </div>
     </form>
   )
