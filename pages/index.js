4fa7fb0b46e2963d6e706e1adb6404e02789f9e9
Chloe Nott <chloe@nott.email>
2022-01-23 01:25:54 -0800

    Form now calculates random cookie sales for each hour based on form inputs. These results update reports state which the table then displays on the page.

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function CookieStandAdmin() {
       <Header title="Cookie Stand Admin" />
 
       <main className="p-6 bg-emerald-50">
-        <CreateForm handleAddReport={handleAddReport} />
+        <CreateForm handleAddReport={handleAddReport} hours={hours} />
         <ReportTable hours={hours} reports={reports} />
       </main>
 
@@ -58,15 +58,26 @@ function Footer() {
   )
 }
 
-function CreateForm({handleAddReport}) {
+function CreateForm({handleAddReport, hours}) {
 
   const hourly_sales = [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
 
   function handleSubmit(event) {
     event.preventDefault()
+
+    let hourlyData = []
+    for (let hour in hours) {
+      max_cust_per_hour = parseFloat(event.target.max_cust_per_hour.value)
+      min_cust_per_hour = parseFloat(event.target.min_cust_per_hour.value)
+      avg_cookies_per_sale = parseFloat(event.target.avg_cookies_per_sale.value)
+
+      let hourData = Math.round((Math.random() * (max_cust_per_hour - min_cust_per_hour) + min_cust_per_hour) * avg_cookies_per_sale)
+      hourlyData.push(hourData)
+    }
+
     let report = {
       name: event.target.location.value,
-      hourlyData: hourly_sales
+      hourlyData: hourlyData
     }
     handleAddReport(report)
   }
@@ -83,15 +94,15 @@ function CreateForm({handleAddReport}) {
       <div className="flex mt-5">
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block mx-auto text-center">Minimum Customers per Hour</label>
-          <input name="min_cust_per_hour" className="w-full"></input>
+          <input id="min_cust_per_hour" type="text" className="w-full"></input>
         </div>
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block mx-auto text-center">Maximum Customers per Hour</label>
-          <input name="max_cust_per_hour" className="w-full"></input>
+          <input id="max_cust_per_hour" type="text" className="w-full"></input>
         </div>
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block text-center">Average Cookies per Sale</label>
-          <input name="avg_cookies_per_sale" className="w-full"></input>
+          <input id="avg_cookies_per_sale" type="text" className="w-full"></input>
         </div>
         <div className="flex items-center justify-center w-1/4 p-2 mx-1 my-2 rounded bg-emerald-500">
           <button className="w-1/4 mx-1 bg-emerald-500" type="submit">Create</button>
@@ -104,11 +115,15 @@ function CreateForm({handleAddReport}) {
 function ReportTable({hours, reports}) {
   return (
     <table>
-      <tr>
-        <th>Location</th>
-        {hours.map(hour => <th key={hour}>{hour}</th>)}
-      </tr>
-      {reports.map(report => <ResultsRow key={report.name} report={report}/>)}
+      <thead>
+        <tr>
+          <th>Location</th>
+          {hours.map(hour => <th key={hour}>{hour}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {reports.map(report => <ResultsRow key={report.name} report={report}/>)}
+      </tbody>
     </table>
   )
 }
