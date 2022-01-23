c7b4a2e949a944de6527f8ea18d2468ea3846621
Chloe Nott <chloe@nott.email>
2022-01-23 01:03:38 -0800

    Added basic form functionality. Form is able to update reports on CookieStandAdmin which flows down to the table for display. Form uses hard coded data.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { render } from 'react-dom'
+import { useState } from 'react'
 
 export default function CookieStandAdmin() {
 
-  // Hours, seattle, wenatchee, and reports are temporary sample data to demonstrate functionality of passing data to child components.
   let hours = ['6am','7am','8am','9am']
 
   let seattle = {
@@ -17,7 +17,11 @@ export default function CookieStandAdmin() {
     hourlyData: ['3', '3', '1', '9']
   }
 
-  let reports = [seattle, wenatchee]
+  const [reports, setReports] = useState([seattle, wenatchee])
+
+  function handleAddReport(report) {
+    setReports([...reports, report])
+  }
 
   return (
     <div>
@@ -28,8 +32,8 @@ export default function CookieStandAdmin() {
       <Header title="Cookie Stand Admin" />
 
       <main className="p-6 bg-emerald-50">
-        <CreateForm />
-        <ReportTable hours={hours} reports={reports}/>
+        <CreateForm handleAddReport={handleAddReport} />
+        <ReportTable hours={hours} reports={reports} />
       </main>
 
       <Footer />
@@ -55,31 +59,43 @@ function Footer() {
   )
 }
 
-function CreateForm() {
+function CreateForm({handleAddReport}) {
+
+  const hourly_sales = [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    let report = {
+      name: event.target.location.value,
+      hourlyData: hourly_sales
+    }
+    handleAddReport(report)
+  }
+
   return (
-    <form className="w-8/12 px-3 pb-2 mx-auto text-sm rounded-lg bg-emerald-300">
+    <form className="w-8/12 px-3 pb-2 mx-auto text-sm rounded-lg bg-emerald-300" onSubmit={handleSubmit} >
       <h2 className="p-3 text-xl text-center">Create Cookie Stand</h2>
 
       <div className="flex m-2">
         <label className="pr-2">Location</label>
-        <input name="location" value="" className="flex-grow"></input>
+        <input id="location" type="text" className="flex-grow" required></input>
       </div>
 
       <div className="flex mt-5">
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block mx-auto text-center">Minimum Customers per Hour</label>
-          <input name="min_cust_per_hour" value="2" className="w-full"></input>
+          <input name="min_cust_per_hour" className="w-full"></input>
         </div>
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block mx-auto text-center">Maximum Customers per Hour</label>
-          <input name="max_cust_per_hour" value="2" className="w-full"></input>
+          <input name="max_cust_per_hour" className="w-full"></input>
         </div>
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-200">
           <label className="block text-center">Average Cookies per Sale</label>
-          <input name="avg_cookies_per_sale" value="2" className="w-full"></input>
+          <input name="avg_cookies_per_sale" className="w-full"></input>
         </div>
         <div className="flex items-center justify-center w-1/4 p-2 mx-1 my-2 rounded bg-emerald-500">
-          <button className="w-1/4 mx-1 bg-emerald-500">Create</button>
+          <button className="w-1/4 mx-1 bg-emerald-500" type="submit">Create</button>
         </div>
       </div>
     </form>
