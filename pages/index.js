512c622b94399099718914b86db6ad2b987afe37
Chloe Nott <chloe@nott.email>
2022-01-24 12:51:09 -0800

    Added LoginForm component and styling. Display of login or cookiestandadmin depends on user variable. Todo: user is currently hardcoded.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,16 @@ import CreateForm from "../components/CreateForm";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function CookieStandAdmin() {
+export default function Home() {
+  const user = ''
+  return (
+    <div>
+      {user ? <CookieStandAdmin /> : <LoginForm />}
+    </div>
+  )
+}
+
+function CookieStandAdmin() {
   const [reports, setReports] = useState([]);
 
   function handleAddReport(report) {
@@ -29,4 +38,22 @@ export default function CookieStandAdmin() {
       <Footer reports={reports} />
     </div>
   );
+}
+
+function LoginForm() {
+  return (
+    <div className="w-8/12 m-3 mx-auto border-2 border-solid p-7 rounded-xl bg-emerald-300 border-emerald-500">
+      <form>
+        <fieldset>
+          <label className="block m-2 mt-0 font-bold text-center" htmlFor="username">USER NAME</label>
+          <input className="block w-full p-2 m-2 mx-auto" type="text" name="username" placeholder="User Name"></input>
+          
+          <label className="block m-2 mt-5 font-bold text-center" htmlFor="password">PASSWORD</label>
+          <input className="block w-full p-2 m-2 mx-auto" type="password" name="password" placeholder="password"></input>
+
+          <button className="block w-full p-4 mx-auto mt-12 rounded bg-emerald-500">SIGN IN</button>
+        </fieldset>
+      </form>
+    </div>
+  )
 }
